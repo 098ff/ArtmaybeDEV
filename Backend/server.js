7dcb301432fb309b23b27ed3a2558b00eedb8b2b
098ff <chanatdakc@gmail.com>
2025-10-25 20:16:12 +0700

    feat: apply security lib

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require("./config/db");
+// Require Sanitizer for not allow Nosql query when calls apis
+const mongoSanitize = require('@exortek/express-mongo-sanitize');
+// Require Helmet for more security headers
+const helmet = require('helmet');
+// Require XSS for not allow 'script' from Hacker when POST apis
+const {xss} = require('express-xss-sanitizer');
+// Require RateLimit for limit number of calling apis
+const rateLimit = require('express-rate-limit');
+// Require HPP for not allow param pollution
+const hpp = require('hpp');
 // Require cors for enable React to communicate with Nodejs
 const cors = require('cors');
 
@@ -17,6 +27,26 @@ app.use(express.json());
 // Cookie parser
 app.use(cookieParser());
 
+// Mongo Sanitizer
+app.use(mongoSanitize());
+
+// Helmet
+app.use(helmet());
+
+// XSS
+app.use(xss());
+
+// Rate Limit
+const limiter = rateLimit({
+    windowsMs: 10*60*1000, // 10 mins
+    max: 100
+});
+app.use(limiter);
+
+// HPP
+app.use(hpp());
+
+// Cors
 app.use(cors());
 
 connectDB();
