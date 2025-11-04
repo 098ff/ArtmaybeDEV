b83ac29da906dbbd66a64cb317dca65a86c758b1
098ff <chanatdakc@gmail.com>
2025-11-04 23:55:46 +0700

    refactor: remove Register since we reg in postman already

--- Frontend/src/features/auth/authSlice.js
+++ Frontend/src/features/auth/authSlice.js
@@ -12,27 +12,12 @@ const initialState = {
     message: ''
 }
 
-// Register user
-export const register = createAsyncThunk('auth/register', async(user, thunkAPI) => {
-
-    console.log(user);
-
-    try {
-        // Connect with api
-        return await authService.register(user);
-    }
-    catch (error) {
-        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
-        return thunkAPI.rejectWithValue(message);
-    }
-});
-
 // Login user
 export const login = createAsyncThunk('auth/login', async(user, thunkAPI) => {
     try {
         return await authService.login(user);
     }
-    catch {
+    catch (error) {
         const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
         return thunkAPI.rejectWithValue(message);
     }
@@ -55,21 +40,7 @@ export const authSlice = createSlice({
         }
     },
     extraReducers: (builder) => {
-        builder.addCase(register.pending, (state) => {
-            state.isLoading = true;
-        })
-        .addCase(register.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isSuccess = true;
-            state.user = action.payload; 
-        })
-        .addCase(register.rejected, (state, action) => {
-            state.isLoading = false;
-            state.isError = true;
-            state.message = action.payload;
-            state.user = null;
-        })
-        .addCase(login.pending, (state) => {
+        builder.addCase(login.pending, (state) => {
             state.isLoading = true;
         })
         .addCase(login.fulfilled, (state, action) => {
