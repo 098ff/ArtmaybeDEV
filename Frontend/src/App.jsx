158c61ca996576ae3651dc82dedde7e1a2b07da8
098ff <chanatdakc@gmail.com>
2025-11-07 23:20:13 +0700

    chore: del unuse code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,15 +6,11 @@ import Header from './components/Header';
 import Companies from './pages/Companies';
 import Favorites from './pages/Favorites';
 import LoginPage from './pages/Login';
-import { use } from 'react';
 
 const PrivateRoutes = ({ allowedRoles }) => {
   const user = JSON.parse(localStorage.getItem('user'));
   const token = user?.token;
   const role = user?.role;
-
-  console.log(user);
-  console.log(role);
 
   if (!token) {
     return <div className='go-login-text'>Please login to start</div>;
